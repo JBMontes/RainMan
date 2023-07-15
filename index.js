7ff0a00ffc10219decfb310c571a594e0ae3ba3e
Jeremy Montes <jeremymontes@pursuit.org>
2023-07-14 22:40:35 -0400

    created img for rain man

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
+function displayCard(word) {
+  let card = document.querySelector(".card");
+  let words = document.querySelector(".word");
+  let letterInput = document.querySelector(".letterInput");
 
-function displayCard(word){
-    let card = document.querySelector(".card")
-    let words = document.querySelector(".word")
-let letterInput = document.querySelector(".letterInput")
+  for (let letter of word) {
+    let gameWord = document.createElement("p");
+    gameWord.innerText = letter.toUpperCase();
 
-    for(let letter of word){
-       
-        let gameWord = document.createElement("p")
-        gameWord.innerText = letter.toUpperCase()
-        
-        words.append(gameWord)
-        
+    words.append(gameWord);
 
-        letterSplit = letter.split("")
-        letterSplit.forEach((singleLetter)=> {
-        let single = document.createElement("p")
-        single.classList.add("letter")
-        let dash = " "
-         single.innerText = dash
-         
-        letterInput.append(single)
-            card.append(letterInput)
-        })
-       
-    }
-   
+    letterSplit = letter.split("");
+    letterSplit.forEach((singleLetter) => {
+      let single = document.createElement("p");
+      single.classList.add("letter");
+      single.innerText = singleLetter.toUpperCase();
+      letterInput.append(single);
+      card.append(letterInput);
+
+      let alphabet = document.querySelectorAll("li");
+      for (let letter of alphabet) {
+        letter.addEventListener("click", (e) => {
+          if (e.target.innerText === single.innerText) {
+            single.style.color = "white";
+            e.target.style.color = "transparent";
+          }
+        });
+      }
+    });
+  }
 }
 
 fetch("https://random-word-api.herokuapp.com/word")
-.then((response) => response.json())
-.then((JSONresponse) => (displayCard(JSONresponse)))
-.catch((err) => {
+  .then((response) => response.json())
+  .then((JSONresponse) => displayCard(JSONresponse))
+  .catch((err) => {
     console.log(err);
   });
+
+ 
